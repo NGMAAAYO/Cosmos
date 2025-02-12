a2a8beb2235ec2a91684c69bf7dfb2bef3655989
Misaka17032 <970143129@qq.com>
2025-02-12 16:08:25 -0600

    cancel map count limitation

--- app.py
+++ app.py
@@ -336,8 +336,6 @@ async def start_match(request: Request,
         return templates.TemplateResponse("dashboard.html", {"request": request, "error": "对手尚未上传玩家代码。", "user": user})
 
     available_maps = list(MAPS_FOLDER.glob("*.json"))
-    if len(available_maps) < 3:
-        return templates.TemplateResponse("dashboard.html", {"request": request, "error": "没有足够的地图。", "user": user})
     chosen_maps = random.sample(available_maps, 3)
     players = [username, opponent]
 
